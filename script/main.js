cfa3a0e273160f743752ad608c229d6ddd2bed7d
k1oske <henryschleining@gmail.com>
2022-11-24 16:46:32 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,11 +1,9 @@
 const widget = new ListWidget()
 const now = Date.now()
-var time = 0
 
 widget.backgroundColor = Color.lightGray()
 
-time += 1
-widget.addText(time.toString())
+widget.addText(now.toString())
 widget.refreshAfterDate = new Date(now + 100)
 
 Script.setWidget(widget)
