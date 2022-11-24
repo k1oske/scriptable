e20b7ef2766690eedae6333b9910bf2f36ed824a
k1oske <henryschleining@gmail.com>
2022-11-24 16:32:02 +0100

    refresh test

--- script/main.js
+++ script/main.js
@@ -1,8 +1,11 @@
 const widget = new ListWidget()
 const now = Date.now()
+var time = 0
+
 widget.backgroundColor = Color.lightGray()
 
-widget.addText(Date)
+time += 1
+widget.addText(time)
 widget.refreshAfterDate = new Date(now + 100)
 
 Script.setWidget(widget)
