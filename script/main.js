715f85bb348e72eb360c6c0a850473d300e1f6de
k1oske <henryschleining@gmail.com>
2022-11-24 17:08:58 +0100

    Revert "Update main.js"
    This reverts commit 3a17d02aed602638403c79c89c5585e2f8135861.

--- script/main.js
+++ script/main.js
@@ -1,16 +1,10 @@
 const w = new ListWidget()
 const now = Date.now()
-var present = Date.now()
 
 w.backgroundColor = Color.lightGray()
 
 w.addText(now.toString())
 w.refreshAfterDate = new Date(now)
-
-while (true){
-    present = Date.now()
-    console.log(present.toString())
-}
 
 Script.setWidget(w)
 Script.complete()
