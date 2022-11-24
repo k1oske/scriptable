8c32f139347a65c57e79a2aa0a29ba918cf4bccb
k1oske <henryschleining@gmail.com>
2022-11-24 17:14:23 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -6,6 +6,10 @@ w.backgroundColor = Color.lightGray()
 w.addText(now.toString())
 w.refreshAfterDate = new Date(now)
 
+for (i = 100; i > 0; i--){
+    console.log(i)
+}
+
 Script.setWidget(w)
 Script.complete()
 w.presentMedium()
