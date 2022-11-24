3a17d02aed602638403c79c89c5585e2f8135861
k1oske <henryschleining@gmail.com>
2022-11-24 17:07:44 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,10 +1,16 @@
 const w = new ListWidget()
 const now = Date.now()
+var present = Date.now()
 
 w.backgroundColor = Color.lightGray()
 
 w.addText(now.toString())
 w.refreshAfterDate = new Date(now)
+
+while (true){
+    present = Date.now()
+    console.log(present.toString())
+}
 
 Script.setWidget(w)
 Script.complete()
