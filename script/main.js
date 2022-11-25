5c15527798dcb9b30ef217a76792ee9953348b03
k1oske <henryschleining@gmail.com>
2022-11-25 17:18:25 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -4,12 +4,12 @@ var date = new Date
 
 w.backgroundColor = Color.lightGray()
 
-date = df.date(date)
+date = df.string(date.toDateString)
 w.addText("minutes: " + date)
 w.refreshAfterDate = new Date(date)
 
 for (i = 100; i > 0; i--){
-    date = df.date(date)
+    date = df.string(date.toDateString)
     console.log("minutes: " + date)
 }
 
