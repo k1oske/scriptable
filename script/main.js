9792f3aeb1a155ebe6fb3a8cab8fe8fb5fd39097
k1oske <henryschleining@gmail.com>
2022-11-25 17:23:15 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ const df = new DateFormatter()
 var date = new Date()
 
 w.backgroundColor = Color.lightGray()
-
+console.log(date)
 date = df.date(date.toDateString())
 w.addText("minutes: " + date)
 w.refreshAfterDate = new Date(date)
