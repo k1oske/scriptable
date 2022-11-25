0e86b2de36764808c02e11d65722f5e03dc0cc08
k1oske <henryschleining@gmail.com>
2022-11-25 17:06:48 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -7,7 +7,7 @@ w.addText("minutes: " + date.toString())
 w.refreshAfterDate = new Date(date)
 
 for (i = 100; i > 0; i--){
-    date = Date.getMinutes()
+    date = Date.getMinutes
     console.log("minutes: " + date)
 }
 
