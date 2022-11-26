9c7c7f1874c8255eb2c8820d72178449cd725276
k1oske <henryschleining@gmail.com>
2022-11-26 11:08:49 +0100

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,11 +1,12 @@
 const w = new ListWidget()
 const df = new DateFormatter()
 var date = new Date()
+var mins
 
 w.backgroundColor = Color.lightGray()
 console.log(date.toDateString)
 
-date = df.date(date.toDateString())
+mins = date.getMinutes()
 w.addText("minutes: " + date)
 w.refreshAfterDate = new Date(date)
 
