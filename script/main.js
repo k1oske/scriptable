3ec691481f235f6f4ce15d26d0ca538727caf60a
k1oske <henryschleining@gmail.com>
2022-12-02 18:39:29 +0100

    sync

--- script/main.js
+++ script/main.js
@@ -1,12 +1,23 @@
 const w = new ListWidget()
+const fm = FileManager.iCloud()
+const ipath = "/private/var/mobile/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents/config.json"
+
 var date = new Date()
 var mins = date.getMinutes()
+var hours = date.getHours()
+var fcont
+
+if(fm.FileExists(ipath)){
+    fcont = fm.readString(ipath)
+    fm.writeString(ipath, mins.toString())
+}
+else{console.log("file does not exit")}
+
+fcont = mins - fcont
 
 w.addText("minutes: " + mins)
+w.addText("last refresh: " + fcont + " minutes ago")
 w.backgroundColor = Color.lightGray()
-
-console.log(mins)
-console.log(date)
 
 w.refreshAfterDate = new Date(date)
 w.presentMedium()
